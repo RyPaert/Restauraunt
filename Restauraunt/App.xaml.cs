709d95cbea8cca5617f52063e0c19784d0fffc25
Ry <thomashenrypaert@gmail.com>
2025-03-28 07:36:43 +0200

    Initial commit

---
+++ Restauraunt/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Restauraunt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Restauraunt/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Restauraunt
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
